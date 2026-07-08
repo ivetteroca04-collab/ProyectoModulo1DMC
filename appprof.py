3f2988f03c2aebe92e2e823f25387668f6abde1c
ivetteroca04-collab <ivetteroca04@gmail.com>
2026-07-08 00:52:19 -0300

    Update appprof.py

--- appprof.py
+++ appprof.py
@@ -1,10 +1,11 @@
 import streamlit as st
+
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
 valor_inicial = st.number_input("Ingrese el valor inicial")
 valor_final = st.number_input("Ingrese el valor final")
 
-lista numérica = list(range(valor_inicial, valor_final))
+lista_numerica = list(range(int(valor_inicial), int(valor_final)))
 
-st.write(lista_numérica)
+st.write(lista_numerica)
