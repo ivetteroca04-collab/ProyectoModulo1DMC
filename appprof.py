08dbf128d120b7abe8d548efe0342ef8056802d2
ivetteroca04-collab <ivetteroca04@gmail.com>
2026-07-08 00:28:26 -0300

    Update appprof.py

--- appprof.py
+++ appprof.py
@@ -3,3 +3,6 @@ st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
 valor_inicial = st.number_input("Ingrese el valor inicial")
+valor_final = st.number_input("Ingrese el valor final")
+
+lista numérica = list(range(valor_inicial, valor_final))
